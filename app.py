125d8e496f99051f503470f64112ed07cea30465
Diez-w <diegozr614@gmail.com>
2025-05-06 20:09:57 -0500

    Fix: corregido __name__ mal escrito

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def recibir():
 
     return "Procesado"
 
-if _name_ == '_main_':
+if __name__ == '_main_':
     import os
 port = int(os.environ.get("PORT", 5000))
 app.run(host='0.0.0.0', port=port)
